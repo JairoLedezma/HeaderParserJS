4177414ac7e60a761f4af2e0886c53f04d629c40
Jairo Ledezma <ledezmajairo20@gmail.com>
2021-07-19 14:02:36 -0500

    Header Parser
    Simple api to get info from the header in a get request

---
+++ boilerplate-project-headerparser/server.js
@@ -0,0 +1,50 @@
+// server.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+const { lookup } = require('geoip-lite');
+const router = express.Router();
+useragent = require('express-useragent')
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+
+app.get("/api/whoami", (req, res)=>{
+    const ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+ 
+ua = req.headers['user-agent'];
+
+ resultDate = { 
+   "ipaddress": ipaddress, 
+   "language": req.headers["accept-language"],
+   "software": ua
+ }
+  res.json(resultDate);
+});
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
